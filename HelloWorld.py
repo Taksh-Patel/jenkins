265db8efb664c129cb8b5a0adc5072210cb7b5ca
Taksh-Patel <74257970+Taksh-Patel@users.noreply.github.com>
2022-03-21 19:45:25 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,3 @@
 #! /usr/bin/python3
-#This Program will print Hello World
-print("Hello World\n")
-print("new commit")
-print("Webhook")
+# This Phython program will print Hellow World...
+print("Hello World Demo...08 Feb 2022 \n")
